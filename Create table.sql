757eb76cad5dec13e3a44a1b7ca3aaadef072e3a
Mikel <mikelvallejo9@gmail.com>
2021-05-09 18:58:09 +0200

    update

---
+++ Create table.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE sharks (
+    Case_number VARCHAR(255) NOT NULL,
+    Date VARCHAR(255) NOT NULL,
+    Year FLOAT NOT NULL,
+    Type VARCHAR(255) NOT NULL,
+    Country VARCHAR(255) NOT NULL,
+    Area VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    Activity VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Sex VARCHAR(255) NOT NULL,
+    Age VARCHAR(255) NOT NULL,
+    Injury VARCHAR(255) NOT NULL,
+    Fatal VARCHAR(255) NOT NULL,
+    Time VARCHAR(255) NOT NULL,
+    Species VARCHAR(255) NOT NULL,
+    Investigator VARCHAR(255) NOT NULL,
+    pdf VARCHAR(255) NOT NULL,
+    href_formula VARCHAR(255) NOT NULL,
+    href VARCHAR(255) NOT NULL,
+    Case_n1 VARCHAR(255) NOT NULL,
+    Case_n2 VARCHAR(255) NOT NULL,
+    original_order Float NOT NULL,
+    unnamed22 VARCHAR(255) NOT NULL,
+    unnamed23 VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Case_number)
+);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
-# TL Project
+## Introduction
+
+On this project, we are going to focus on the transformation and load of an specific dataframe. After that, we will be able to analyze data and get insights about shark attacks around the world in past years.
+
+  
+
+## Instructions for the analyst - how data was modified
+
+At first the database was a bit messy and full of nulls. These are the principal changes were made during the transformation and data cleaning process:
+
+ - All the rows that had all their values at null, were deleted.
+ - After that Df was reduced by deleting every row with les than 3 no-null values.
+ - No columns were deleted during this aproach.
+ - You can find null values in the columns with the value 'Unknown', with 3 exceptions: pdf column ('no pdf') and both href columns ('no link')
